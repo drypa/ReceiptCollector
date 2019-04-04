f0562f332fbe631858d0c506b18e94c683eeb677
DRypa <dr.drypa.k@mail.ru>
2019-04-04 22:07:12 +0300

    stub for nalog.ru entrypoint

---
+++ stub/stub_app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+)
+
+func main() {
+	baseDir := "./dump/"
+	files := getFiles(baseDir)
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		if request.Method != http.MethodGet {
+			writer.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		fileNumber := rand.Intn(len(files))
+		randomFile := files[fileNumber]
+		content, err := ioutil.ReadFile(baseDir + randomFile.Name())
+		check(err)
+		_, _ = writer.Write(content)
+
+	})
+
+	fmt.Println(http.ListenAndServe(":9999", nil))
+}
+
+func getFiles(directory string) []os.FileInfo {
+	files, err := ioutil.ReadDir(directory)
+	check(err)
+
+	return files
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
