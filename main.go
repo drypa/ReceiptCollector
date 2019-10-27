8edaf9b668ed5c519c37456afdca1318e1cf9ee8
DRypa <dr.drypa.k@mail.ru>
2019-10-27 17:14:14 +0300

    unused code removed

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/adjust/redismq"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 	"os"
 	"receipt_collector/auth"
@@ -23,8 +21,6 @@ var login = os.Getenv("NALOGRU_LOGIN")
 var password = os.Getenv("NALOGRU_PASS")
 var baseAddress = os.Getenv("NALOGRU_BASE_ADDR")
 
-const dumpDirectory = "./stub/dump/"
-
 var mongoUrl = os.Getenv("MONGO_URL")
 
 var mongoUser = os.Getenv("MONGO_LOGIN")
@@ -33,16 +29,12 @@ var mongoSecret = os.Getenv("MONGO_SECRET")
 var redisHost = os.Getenv("REDIS_HOST")
 var redisPort = os.Getenv("REDIS_PORT")
 
-var rawReceiptQueue = redismq.CreateQueue(redisHost, redisPort, "", 6, "raw-receipts")
-var requestsQueue = redismq.CreateQueue(redisHost, redisPort, "", 6, "requests")
-
 func main() {
 	nalogruClient := nalogru_client.NalogruClient{BaseAddress: baseAddress, Login: login, Password: password}
 	marketsController := markets.New(mongoUrl, mongoUser, mongoSecret)
 	ctx := context.Background()
 	go sendOdfsRequest(ctx, nalogruClient)
 	go getReceipt(ctx, nalogruClient)
-	//go consumeRawReceipts(rawReceiptQueue)
 	router := mux.NewRouter()
 	router.HandleFunc("/api/market", marketsController.MarketsBaseHandler)
 	router.HandleFunc("/api/market/{id:[a-zA-Z0-9]+}", marketsController.ConcreteMarketHandler).Methods(http.MethodPut, http.MethodGet, http.MethodDelete)
@@ -122,56 +114,8 @@ func registerUnauthenticatedRoutes(router *mux.Router) {
 
 }
 
-func saveResponse(queue *redismq.Queue, response []byte) {
-	err := queue.Put(string(response))
-	check(err)
-}
-
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func consumeRawReceipts(rawQueue *redismq.Queue) {
-	consumer, err := rawQueue.AddConsumer("receipt-parser")
-	check(err)
-	defer consumer.Quit()
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo_client.GetMongoClient(mongoUrl, mongoUser, mongoSecret)
-	check(err)
-	defer func() {
-		err := client.Disconnect(ctx)
-		fmt.Printf("error while disconnect %s", err)
-	}()
-	collection := client.Database("receipt_collection").Collection("receipts")
-
-	if consumer.HasUnacked() {
-		unacked, err := consumer.GetUnacked()
-		check(err)
-		processReceipt(unacked, collection)
-		err = unacked.Ack()
-		check(err)
-	}
-
-	for {
-		message, err := consumer.Get()
-		check(err)
-
-		processReceipt(message, collection)
-		err = message.Ack()
-		check(err)
-	}
-}
-
-func processReceipt(message *redismq.Package, collection *mongo.Collection) {
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	receipt, err := receipts.ParseReceipt([]byte(message.Payload))
-	fmt.Println(receipt.String())
-	for i := 0; i < len(receipt.Items); i++ {
-		fmt.Println(receipt.Items[i].String())
-	}
-	_, err = collection.InsertOne(ctx, receipt)
-	check(err)
-
 }
