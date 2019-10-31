6500fb2d747242a3311e07f6ba69434a7fc4e88d
DRypa <dr.drypa.k@mail.ru>
2019-10-31 09:04:17 +0300

    interval odfs requests worker

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
 	"os"
 	"receipt_collector/auth"
@@ -26,15 +27,27 @@ var mongoUrl = os.Getenv("MONGO_URL")
 var mongoUser = os.Getenv("MONGO_LOGIN")
 var mongoSecret = os.Getenv("MONGO_SECRET")
 
-var redisHost = os.Getenv("REDIS_HOST")
-var redisPort = os.Getenv("REDIS_PORT")
+const intervalEnvironmentVariable = "GET_RECEIPT_WORKER_INTERVAL"
+
+var workerIntervalString = os.Getenv(intervalEnvironmentVariable)
 
 func main() {
 	nalogruClient := nalogru_client.NalogruClient{BaseAddress: baseAddress, Login: login, Password: password}
 	marketsController := markets.New(mongoUrl, mongoUser, mongoSecret)
 	ctx := context.Background()
-	go sendOdfsRequest(ctx, nalogruClient)
+
+	processingInterval, err := time.ParseDuration(workerIntervalString)
+	if err != nil {
+		fmt.Printf("invalid '%s' value: %s", intervalEnvironmentVariable, workerIntervalString)
+	}
+
+	go sendOdfsRequestWorkerStart(ctx, nalogruClient, processingInterval)
 	go getReceipt(ctx, nalogruClient)
+
+	fmt.Println(startServer(marketsController))
+}
+
+func startServer(marketsController markets.Controller) error {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/market", marketsController.MarketsBaseHandler)
 	router.HandleFunc("/api/market/{id:[a-zA-Z0-9]+}", marketsController.ConcreteMarketHandler).Methods(http.MethodPut, http.MethodGet, http.MethodDelete)
@@ -46,7 +59,7 @@ func main() {
 	http.Handle("/", auth.RequireBasicAuth(router))
 	address := ":8888"
 	fmt.Printf("Starting http server at: \"%s\"...", address)
-	fmt.Println(http.ListenAndServe(address, nil))
+	return http.ListenAndServe(address, nil)
 }
 
 func getReceipt(ctx context.Context, nalogruClient nalogru_client.NalogruClient) {
@@ -78,7 +91,23 @@ func getReceipt(ctx context.Context, nalogruClient nalogru_client.NalogruClient)
 	check(err)
 }
 
-func sendOdfsRequest(ctx context.Context, nalogruClient nalogru_client.NalogruClient) {
+func sendOdfsRequestWorkerStart(ctx context.Context, nalogruClient nalogru_client.NalogruClient, inteval time.Duration) {
+	ticker := time.NewTicker(inteval)
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Odfs request worker finished")
+			return
+		case <-ticker.C:
+			processRequests(ctx, nalogruClient)
+			break
+		}
+	}
+
+}
+
+func processRequests(ctx context.Context, nalogruClient nalogru_client.NalogruClient) {
 	client, err := mongo_client.GetMongoClient(mongoUrl, mongoUser, mongoSecret)
 	check(err)
 
@@ -89,9 +118,12 @@ func sendOdfsRequest(ctx context.Context, nalogruClient nalogru_client.NalogruCl
 	collection := client.Database("receipt_collection").Collection("receipt_requests")
 	usersReceipt := receipts.UsersReceipt{}
 	err = collection.FindOne(ctx, bson.M{"odfs_requested": false}).Decode(&usersReceipt)
-
+	if err == mongo.ErrNoDocuments {
+		fmt.Println("No Odfs requests required")
+		return
+	}
 	if err != nil {
-		fmt.Printf("error while fetch unprocessed user requests. %s", err)
+		fmt.Printf("error while fetch unprocessed user requests. %s \n", err)
 		return
 	}
 	err = nalogruClient.SendOdfsRequest(usersReceipt.QueryString)
@@ -116,6 +148,7 @@ func registerUnauthenticatedRoutes(router *mux.Router) {
 
 func check(err error) {
 	if err != nil {
+		fmt.Printf("Error occured %v", err)
 		panic(err)
 	}
 }
