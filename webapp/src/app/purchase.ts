f8768d6535e6313676c32660845adbcf3256e464
DRypa <dr.drypa.k@mail.ru>
2019-11-12 19:52:55 +0300

    more receipt details

---
+++ webapp/src/app/purchase.ts
@@ -0,0 +1,6 @@
+export interface Purchase {
+  price: number;
+  sum: number;
+  quantity: number;
+  name: string;
+}
--- webapp/src/app/receipt.ts
+++ webapp/src/app/receipt.ts
@@ -1,17 +1,16 @@
-export class Receipt {
-    id: string;
-    dateTime: string;
-    totalSum: number;
-    retailPlaceAddress: string;
-	userInn: string;
-	// items                []Purchase         `json:"items"`
-	// operator             string             `json:"operator"`
-	// nds18                int32              `json:"nds18"`
-	// nds10                int32              `json:"nds10"`
-	// user                 string             `json:"user"`
-	// cashTotalSum         int32              `json:"cashTotalSum"`
-	// ecashTotalSum        int32              `json:"ecashTotalSum"`
-	// fiscalSign           int64              `json:"fiscalSign"`
-	// fiscalDocumentNumber int64              `json:"fiscalDocumentNumber"`
+import {Purchase} from "./purchase";
 
+export class Receipt {
+  id: string;
+  dateTime: string;
+  totalSum: number;
+  retailPlaceAddress: string;
+  userInn: string;
+  items: Purchase[];
+  cashTotalSum: number;
+  ecashTotalSum: number;
+  user: string;
+  operator: string;
+  nds18: number;
+  nds10: number;
 }
