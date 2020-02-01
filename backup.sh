c38f72240200ae5f87f4fce475f36f5a4d525b6a
DRypa <dr.drypa.k@mail.ru>
2020-02-01 16:54:12 +0300

    add hours, minutes & seconds to backup name for more often backups

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env
 
-docker exec -it  $(docker ps -f "name=mongo" -q) mongodump --out /backup/`date +"%m-%d-%y"` --gzip --db receipt_collection \
+docker exec -it  $(docker ps -f "name=mongo" -q) mongodump --out /backup/`date +"%m-%d-%y-%H-%M-%S"` --gzip --db receipt_collection \
 --username $MONGO_LOGIN --password $MONGO_SECRET --authenticationDatabase admin
