7069b348747b15c3f3f55d1d107c38ac632fd933
Andrey Krasavin <dr.drypa.k@mail.ru>
2020-02-18 09:57:17 +0300

    custom date-time parsing removed
    it is allowed time with and without seconds in receipt

--- backend/nalogru/time_parser.go
+++ backend/nalogru/time_parser.go
@@ -1,28 +1,16 @@
 package nalogru
 
 import (
-	"strconv"
 	"time"
 )
 
 //Парсит строку формата yyyyMMddThhmm
 func parseAsTime(timeString string) (time.Time, error) {
-	year, errYear := strconv.Atoi(timeString[0:4])
-	month, errMonth := strconv.Atoi(timeString[4:6])
-	day, errDay := strconv.Atoi(timeString[6:8])
-	hour, errHour := strconv.Atoi(timeString[9:11])
-	minutes, errMinutes := strconv.Atoi(timeString[11:])
-
-	result := time.Date(int(year), time.Month(int(month)), int(day), int(hour), int(minutes), 0, 0, time.Local)
-
-	return result, firstError([]error{errYear, errMonth, errDay, errHour, errMinutes})
-}
-
-func firstError(errors []error) error {
-	for _, err := range errors {
-		if err != nil {
-			return err
-		}
+	layoutWithSeconds := "20060102T150405"
+	layoutWithoutSeconds := "20060102T1504"
+	res, err := time.ParseInLocation(layoutWithSeconds, timeString, time.Local)
+	if err == nil {
+		return res, err
 	}
-	return nil
+	return time.ParseInLocation(layoutWithoutSeconds, timeString, time.Local)
 }
