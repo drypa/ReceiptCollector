f893c9ad15b4379051d1a945b6c3514bc7c5092f
DRypa <dr.drypa.k@mail.ru>
2020-06-10 22:48:53 +0300

    volume for bot ssl cert

--- backend/main.go
+++ backend/main.go
@@ -53,7 +53,7 @@ func main() {
 	go worker.GetReceiptStart(ctx, settings)
 	generator := login_url.New(openUrl)
 
-	creds, err := credentials.NewServerTLSFromFile("../ssl/certificate.crt", "../ssl/private.key")
+	creds, err := credentials.NewServerTLSFromFile("/etc/ssl/certs/certificate.crt", "/etc/ssl/certs/private.key")
 	if err != nil {
 		log.Fatalf("failed to load TLS keys: %v", err)
 	}
