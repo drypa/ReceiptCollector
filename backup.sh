9f0fbfb2a68039251737abacf0d198e56e5a87ca
DRypa <dr.drypa.k@mail.ru>
2020-10-26 22:31:29 +0300

    fixes to run backup script thought ssh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source .env
 
-docker exec -it  $(docker ps -f "name=mongo" -q) mongodump --out /backup/`date +"%m-%d-%y-%H-%M-%S"` --gzip --db receipt_collection \
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+source $SCRIPT_DIR/.env
+
+docker exec  $(docker ps -f "name=mongo" -q) mongodump --out /backup/`date +"%m-%d-%y-%H-%M-%S"` --gzip --db receipt_collection \
 --username $MONGO_LOGIN --password $MONGO_SECRET --authenticationDatabase admin
