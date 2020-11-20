e39754a34e8f7eef3e79ed45ef8f1df3954b1f75
DRypa <dr.drypa.k@mail.ru>
2020-11-20 13:42:29 +0300

    #48 new ticket response types

--- backend/nalogru/ticket.go
+++ backend/nalogru/ticket.go
@@ -1,6 +1,6 @@
 package nalogru
 
-type Ticket struct {
+type TicketDetails struct {
 	Status    int    `json:"status"`
 	Id        string `json:"id"`
 	Kind      string `json:"kind"`
@@ -8,6 +8,7 @@ type Ticket struct {
 	Qr        string
 	Operation Operation `json:"operation"`
 	Query     Query     `json:"query"`
+	Ticket    *Ticket   `json:"ticket"`
 	Seller    *Seller
 }
 
@@ -29,6 +30,42 @@ type Query struct {
 type Seller struct {
 	Name string `json:"name"`
 	Inn  string `json:"inn"`
+}
+
+type Ticket struct {
+	Document *Document `json:"document"`
+}
+
+type Document struct {
+	Receipt *Receipt `json:"receipt"`
+}
+
+type Receipt struct {
+	DateTime                int64  `json:"dateTime"`
+	CashTotalSum            int    `json:"cashTotalSum"`
+	Code                    int    `json:"code"`
+	CreditSum               int    `json:"creditSum"`
+	EcashTotalSum           int    `json:"ecashTotalSum"`
+	FiscalDocumentFormatVer int    `json:"fiscalDocumentFormatVer"`
+	FiscalDocumentNumber    int    `json:"fiscalDocumentNumber"`
+	FiscalDriveNumber       int64  `json:"fiscalDriveNumber"`
+	FiscalSign              int64  `json:"fiscalSign"`
+	FnsUrl                  string `json:"fnsUrl"`
+	Items                   []Item `json:"items"`
+	KktRegId                string `json:"kktRegId"`
+	Nds10                   int64  `json:"nds10"`
+	Nds18                   int64  `json:"nd18"`
+	OperationType           int    `json:"operationType"`
+	Operator                string `json:"operator"`
+	PrepaidSum              int64  `json:"prepaidSum"`
+	ProvisionSum            int64  `json:"provisionSum"`
+	RequestNumber           int    `json:"requestNumber"`
+	RetailPlace             string `json:"retailPlace"`
+	RetailPlaceAddress      string `json:"retailPlaceAddress"`
+	ShiftNumber             int    `json:"shiftNumber"`
+	TotalSum                int64  `json:"totalSum"`
+	User                    string `json:"user"`
+	UserInn                 string `json:"userInn"`
 }
 
 type Item struct {
