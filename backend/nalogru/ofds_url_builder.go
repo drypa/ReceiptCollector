2fc68ea8895867944b168615bd7837e2f34e9695
DRypa <dr.drypa.k@mail.ru>
2020-11-21 16:03:21 +0300

    #48 remove unused methods

--- backend/nalogru/ofds_url_builder.go
+++ backend/nalogru/ofds_url_builder.go
@@ -6,18 +6,6 @@ import (
 	"strings"
 )
 
-func buildOfdsUrl(baseAddress string, parameters qr.Query) string {
-	timeString := parameters.Time.Format("2006-01-02T15:04:05")
-	return fmt.Sprintf("%s/v1/ofds/*/inns/*/fss/%s/operations/%s/tickets/%s?fiscalSign=%s&date=%s&sum=%s",
-		baseAddress,
-		parameters.Fd,
-		parameters.N,
-		parameters.Fp,
-		parameters.FiscalSign,
-		timeString,
-		parameters.Sum)
-}
-
 func buildCheckReceiptUrl(baseAddress string, queryString string) (string, error) {
 	parse, err := qr.Parse(queryString)
 	if err != nil {
