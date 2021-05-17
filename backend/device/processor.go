66c9fa4a710c790e76cf317473efae133c008a4c
DRypa <dr.drypa.k@mail.ru>
2021-05-17 22:35:11 +0300

    processor implementation

--- backend/device/processor.go
+++ backend/device/processor.go
@@ -3,6 +3,7 @@ package device
 import (
 	"context"
 	api "github.com/drypa/ReceiptCollector/api/inside"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 //Processor for actions with devices.
@@ -11,11 +12,37 @@ type Processor struct {
 }
 
 func (p *Processor) GetDevices(request *api.GetDevicesRequest, server api.InternalApi_GetDevicesServer) error {
-	panic("implement me")
+	devices, err := p.r.All(server.Context())
+	if err != nil {
+		return err
+	}
+	for _, v := range devices {
+		err := server.Send(&api.Device{
+			ClientSecret: v.ClientSecret,
+			SessionId:    v.SessionId,
+			RefreshToken: v.RefreshToken,
+			Id:           v.Id.Hex(),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (p *Processor) UpdateDevice(ctx context.Context, request *api.UpdateDeviceRequest) (*api.ErrorResponse, error) {
-	panic("implement me")
+	id, err := primitive.ObjectIDFromHex(request.Device.Id)
+	if err != nil {
+		return nil, err
+	}
+	device := Device{
+		ClientSecret: request.Device.ClientSecret,
+		SessionId:    request.Device.SessionId,
+		RefreshToken: request.Device.RefreshToken,
+		Id:           id,
+	}
+	err = p.r.Update(ctx, &device)
+	return nil, err
 }
 
 //NewProcessor constructs Processor.
