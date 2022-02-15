11f4f729467f6e64419e16147e855a2176fddf90
DRypa <dr.drypa.k@mail.ru>
2022-02-15 23:03:51 +0300

    #84 report client template

---
+++ bot/backend/report/client.go
@@ -0,0 +1,38 @@
+package report
+
+import (
+	"context"
+	inside "github.com/drypa/ReceiptCollector/api/inside"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+)
+
+type Client struct {
+	report *inside.ReportApiClient
+}
+
+func New(backendUrl string, creds credentials.TransportCredentials) *Client {
+	dial, err := grpc.Dial(backendUrl, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Printf("Failed to create connection with %s. Error: %v", backendUrl, err)
+	}
+	report := inside.NewReportApiClient(dial)
+	return &Client{report: &report}
+}
+
+func (c *Client) GetReports(ctx context.Context, in *inside.NoParams, opts ...grpc.CallOption) (inside.ReportApi_GetReportsClient, error) {
+	stream, err := c.GetReports(ctx, in, opts...)
+	if err != nil {
+		log.Fatalf("%v.GetReports() failed with %v", c.report, err)
+	}
+	for {
+		report, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("%v.Recv() failed with %v", stream, err)
+		}
+		log.Printf("Send report %v", *report)
+		//TODO: send message to client
+	}
+
+}
