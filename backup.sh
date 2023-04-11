865531e850ec86c270513f8b5ae0314f5b86e46a
DRypa <dr.drypa.k@mail.ru>
2023-04-11 22:02:19 +0300

    backup directory template changed

--- backup.sh
+++ backup.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source $SCRIPT_DIR/.env
 
-docker exec  $(docker ps -f "name=mongo" -q) mongodump --out /backup/`date +"%m-%d-%y-%H-%M-%S"` --gzip --db receipt_collection \
+docker exec  $(docker ps -f "name=mongo" -q) mongodump --out /backup/`date +"%Y-%m-%d-%H-%M-%S"` --gzip --db receipt_collection \
 --username $MONGO_LOGIN --password $MONGO_SECRET --authenticationDatabase admin
