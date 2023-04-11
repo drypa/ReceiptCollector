50c7f98f123e27999404125bb6d578a6f05fcf81
DRypa <dr.drypa.k@mail.ru>
2023-04-11 22:34:13 +0300

    remove old backups

--- backup.sh
+++ backup.sh
@@ -5,3 +5,5 @@ source $SCRIPT_DIR/.env
 
 docker exec  $(docker ps -f "name=mongo" -q) mongodump --out /backup/`date +"%Y-%m-%d-%H-%M-%S"` --gzip --db receipt_collection \
 --username $MONGO_LOGIN --password $MONGO_SECRET --authenticationDatabase admin
+
+docker exec  $(docker ps -f "name=mongo" -q) find /backup/ -maxdepth 1 -type d -mtime +365 -exec rm -rf {} \;
