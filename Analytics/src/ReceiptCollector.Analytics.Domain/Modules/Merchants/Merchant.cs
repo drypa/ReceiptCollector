7e0cb074a58b29c1f70480917ec3201ee0c623a4
drypa <dr.drypa.k@mail.ru>
2025-10-20 20:26:38 +0300

    extra merchant place removed. now merchant is present by only one class

--- Analytics/src/ReceiptCollector.Analytics.Domain/Modules/Merchants/Merchant.cs
+++ Analytics/src/ReceiptCollector.Analytics.Domain/Modules/Merchants/Merchant.cs
@@ -5,11 +5,7 @@ public sealed class Merchant
     public Guid Id { get; }
     public string Name { get; }
     public MerchantCategory Category { get; private set; } = MerchantCategory.Undefined;
-    public IReadOnlyCollection<Place> Places => _places;
-    private readonly List<Place> _places = new();
+    public string Address { get; }
+    public string Inn { get; }
 
-    public void ChangeCategory(MerchantCategory category)
-    {
-        Category = category;
-    }
 }
--- Analytics/src/ReceiptCollector.Analytics.Domain/Modules/Merchants/Place.cs
+++
@@ -1,9 +0,0 @@
-namespace ReceiptCollector.Analytics.Domain.Modules.Merchants;
-
-public class Place
-{
-    public int Id { get; }
-    public Guid MerchantId { get; }
-    public string Address { get; }
-    public string Inn { get; }
-}
